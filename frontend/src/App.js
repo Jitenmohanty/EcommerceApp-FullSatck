81158ee261dede522ce736b55abe5f5f5afdaa49
Jiten Kumar Mohanty <jitenmohantyaz@gmail.com>
2023-11-24 22:57:06 +0530

    Cart and Checkout container frontend done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
+import CartPage from './pages/CartPage';
+import Checkout from './pages/Checkout';
 import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
+import ProductDetailPage from './pages/ProductDetailPage';
 import SignupPage from './pages/SignUpPage';
 
 import {
@@ -22,6 +25,18 @@ const router = createBrowserRouter([
   {
     path: '/signup',
     element: <SignupPage></SignupPage>,
+  },
+  { 
+    path: '/cart',
+    element: <CartPage></CartPage>,
+  },
+  { 
+    path: '/checkout',
+    element: <Checkout></Checkout>,
+  },
+  { 
+    path: '/product-detail',
+    element: <ProductDetailPage></ProductDetailPage>,
   },
 ]);
 
