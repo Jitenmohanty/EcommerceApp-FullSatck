289a087f0570279744f2ca9902801cfd7cb36e91
Jiten Kumar Mohanty <jitenmohantyaz@gmail.com>
2023-11-28 20:24:36 +0530

    Cart Items Validation Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,47 +1,74 @@
-import './App.css';
-import Protected from './features/auth/Components/Protected';
-import CartPage from './pages/CartPage';
-import Checkout from './pages/Checkout';
-import Home from './pages/Home';
-import LoginPage from './pages/LoginPage';
-import ProductDetailPage from './pages/ProductDetailPage';
-import SignupPage from './pages/SignUpPage';
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
+import Protected from "./features/auth/Components/Protected";
+import CartPage from "./pages/CartPage";
+import Checkout from "./pages/Checkout";
+import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import ProductDetailPage from "./pages/ProductDetailPage";
+import SignupPage from "./pages/SignUpPage";
 
 import {
   createBrowserRouter,
   RouterProvider,
   Route,
   Link,
-} from 'react-router-dom';
+} from "react-router-dom";
+import { useEffect } from "react";
+import { fetchItemByUserIdAsync } from "./features/cart/cartSlice";
+import { selectLoggedInUser } from "./features/auth/authSlice";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element:<Protected><Home></Home></Protected>,
+    path: "/",
+    element: (
+      <Protected>
+        <Home></Home>
+      </Protected>
+    ),
   },
   {
-    path: '/login',
+    path: "/login",
     element: <LoginPage></LoginPage>,
   },
   {
-    path: '/signup',
+    path: "/signup",
     element: <SignupPage></SignupPage>,
   },
-  { 
-    path: '/cart',
-    element: <Protected><CartPage></CartPage></Protected>,
+  {
+    path: "/cart",
+    element: (
+      <Protected>
+        <CartPage></CartPage>
+      </Protected>
+    ),
   },
-  { 
-    path: '/checkout',
-    element: <Protected><Checkout></Checkout></Protected>,
+  {
+    path: "/checkout",
+    element: (
+      <Protected>
+        <Checkout></Checkout>
+      </Protected>
+    ),
   },
-  { 
-    path: '/product-detail/:id',
-    element: <Protected><ProductDetailPage></ProductDetailPage></Protected>,
+  {
+    path: "/product-detail/:id",
+    element: (
+      <Protected>
+        <ProductDetailPage></ProductDetailPage>
+      </Protected>
+    ),
   },
 ]);
 
 function App() {
+  const dispatch = useDispatch();
+  const user = useSelector(selectLoggedInUser);
+  useEffect(() => {
+    if (user) {
+      dispatch(fetchItemByUserIdAsync(user.id));
+    }
+  }, [dispatch, user]);
   return (
     <div className="App">
       <RouterProvider router={router} />
--- frontend/src/features/cart/cartSlice.js
+++ frontend/src/features/cart/cartSlice.js
@@ -1,22 +1,31 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { fetchCount } from './cartApi';
+import { addToCart, fetchItemByUserId } from './cartApi';
 
 const initialState = {
-  value: 0,
+  items: [],
   status: 'idle',
+  value:0
 };
 
-export const incrementAsync = createAsyncThunk(
-  'counter/fetchCount',
-  async (amount) => {
-    const response = await fetchCount(amount);
+export const addToCartAsync = createAsyncThunk(
+  'cart/addToCart',
+  async (item) => {
+    const response = await addToCart(item);
+    // The value we return becomes the `fulfilled` action payload
+    return response.data;
+  }
+);
+export const fetchItemByUserIdAsync = createAsyncThunk(
+  'cart/fetchItemByUserId',
+  async (userId) => {
+    const response = await fetchItemByUserId(userId);
     // The value we return becomes the `fulfilled` action payload
     return response.data;
   }
 );
 
 export const counterSlice = createSlice({
-  name: 'counter',
+  name: 'cart',
   initialState,
   reducers: {
     increment: (state) => {
@@ -25,18 +34,25 @@ export const counterSlice = createSlice({
   },
   extraReducers: (builder) => {
     builder
-      .addCase(incrementAsync.pending, (state) => {
+      .addCase(addToCartAsync.pending, (state) => {
         state.status = 'loading';
       })
-      .addCase(incrementAsync.fulfilled, (state, action) => {
+      .addCase(addToCartAsync.fulfilled, (state, action) => {
         state.status = 'idle';
-        state.value += action.payload;
+        state.items.push(action.payload)
+      })
+      .addCase(fetchItemByUserIdAsync.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchItemByUserIdAsync.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.items = action.payload;
       });
   },
 });
 
 export const { increment } = counterSlice.actions;
 
-export const selectCount = (state) => state.counter.value;
+export const selectCartItem = (state) => state.cart.items;
 
 export default counterSlice.reducer;
